7ee393e0624cc35b1f928f2dd91079f925ad149c
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-01-08 13:48:45 +0100

    Added Lombok dependency

---
+++ backend/src/main/java/dev/itvitae/grocerystore/products/Product.java
@@ -0,0 +1,14 @@
+package dev.itvitae.grocerystore.products;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Product {
+    
+}
