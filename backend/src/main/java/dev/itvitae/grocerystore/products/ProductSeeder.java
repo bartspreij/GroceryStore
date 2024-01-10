ea2abf61b59dd3f73dac263b060b78c3ae106ea4
Tuyan T <tuyantat@gmail.com>
2024-01-10 13:35:27 +0100

    seeder update

--- backend/src/main/java/dev/itvitae/grocerystore/products/ProductSeeder.java
+++ backend/src/main/java/dev/itvitae/grocerystore/products/ProductSeeder.java
@@ -21,10 +21,15 @@ public class ProductSeeder implements CommandLineRunner {
     public void run(String... args) throws Exception {
             String fruit = "Fruit";
             String potassium = "Potassium";
+            String meat = "Meat";
+            String dairy = "Dairy";
 
             saveProduct("Apple", "https://i.imgur.com/TVN1Hs5.jpeg", BigDecimal.valueOf(0.89), fruit);
 
             saveProduct("Banana", "https://i.imgur.com/xhlyEjv.png", BigDecimal.valueOf(1.29), fruit, potassium);
 
+            saveProduct("Gehakt", "https://static.ah.nl/dam/product/AHI_4354523130303233323432?revLabel=1&rendition=800x800_JPG_Q90&fileType=binary", BigDecimal.valueOf(3.49), meat);
+
+            saveProduct("Melk", "https://static.ah.nl/dam/product/AHI_43545239393331383832?revLabel=6&rendition=800x800_JPG_Q90&fileType=binary", BigDecimal.valueOf(1.89), dairy);
     }
 }
