faaba410511cc892d9275485e4e999816ba47371
Melle Gubbels <melle.gubbels@itvitaelearning.nl>
2024-01-11 15:47:15 +0100

    Added pageable queries (all/name/tag)

--- backend/src/main/java/dev/itvitae/grocerystore/tags/TagRepository.java
+++ backend/src/main/java/dev/itvitae/grocerystore/tags/TagRepository.java
@@ -3,7 +3,9 @@ package dev.itvitae.grocerystore.tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public interface TagRepository extends JpaRepository<Tag, Long>{
-
+    Optional<Tag> findByName(String name);
 }
