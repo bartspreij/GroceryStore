366530f6e5097404765da2d43e86ffcb932416d9
Tuyan T <tuyantat@gmail.com>
2024-01-24 15:08:54 +0100

    Finished

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
 import Products from './components/Products';
 import ManageProducts from './components/ManageProducts';
 
