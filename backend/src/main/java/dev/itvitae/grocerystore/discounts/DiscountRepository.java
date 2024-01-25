7c91558a8515cd3ecc87ec5d88a5f4d0a412b610
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-01-25 13:02:31 +0100

    Removed unused code

--- backend/src/main/java/dev/itvitae/grocerystore/discounts/DiscountRepository.java
+++ backend/src/main/java/dev/itvitae/grocerystore/discounts/DiscountRepository.java
@@ -3,14 +3,7 @@ package dev.itvitae.grocerystore.discounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
 @Component
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
-
-//    Iterable<Discount>findByStartDateAfterAndFindByEndDateBefore();
-//
-//    Iterable<Discount>findByStartDateBefore();
-
 
 }
